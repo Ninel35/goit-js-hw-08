60b88d7ce2ab2701c8afd0aeaa76657f0f5fe2cf
Ninel35 <134783428+Ninel35@users.noreply.github.com>
2023-10-07 21:08:20 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const throttle = require('lodash.throttle');
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+
+const onPlay = function (data) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data)); 
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")).seconds);
+
 
